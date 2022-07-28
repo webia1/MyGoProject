094b504f5eec8c647846425d5fec10a44ec465d3
Erkan Buelbuel <webia1.github@gmail.com>
2022-07-28 15:20:06 +0200

    Slices proceeding

--- src/main.go
+++ src/main.go
@@ -1,20 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	_ "reflect"
+)
 
 func main() {
 
 	var x []int
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 10)
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 11)
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 12)
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 13)
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 14)
-	fmt.Println(x, len(x), cap(x))
+	var y = []int{}
 
+	fmt.Println(x, len(x)) // [] 0
+	fmt.Println(y, len(y)) // [] 0
+
+	fmt.Println(x == nil) // true
+	fmt.Println(y == nil) // false
+
+	fmt.Println(reflect.TypeOf(x))                      // []int
+	fmt.Println(reflect.TypeOf(y))                      // []int
+	fmt.Println(reflect.TypeOf(y) == reflect.TypeOf(x)) // true
+
+	fmt.Println(reflect.ValueOf(x).Kind()) // slice
+	fmt.Println(reflect.ValueOf(y).Kind()) // slice
+
+	var v []int
+	z := make([]int, 0, 10)
+
+	fmt.Println(v, len(v), cap(v))
+	fmt.Println(z, len(z), cap(z))
+
+	v = append(v, 3, 5, 7)
+	z = append(x, 3, 5, 7)
+
+	fmt.Println(v, len(v), cap(v))
+	fmt.Println(z, len(z), cap(z))
+
+	fmt.Println("Before Programm End")
 }
