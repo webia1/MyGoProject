a47d331839aa79941afba7c9bfe95c877d3343ba
Erkan Buelbuel <webia1.github@gmail.com>
2022-07-29 11:34:08 +0200

    Slice-Sliding

--- src/main.go
+++ src/main.go
@@ -2,39 +2,19 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	_ "reflect"
 )
 
 func main() {
 
-	var x []int
-	var y = []int{}
+	var x = []int{2, 3, 5, 7, 9}
+	var a = x[:3]
+	var b = x[2:]
+	var c = x[1:4]
+	var d = x[:]
+	var e = x
 
-	fmt.Println(x, len(x)) // [] 0
-	fmt.Println(y, len(y)) // [] 0
-
-	fmt.Println(x == nil) // true
-	fmt.Println(y == nil) // false
-
-	fmt.Println(reflect.TypeOf(x))                      // []int
-	fmt.Println(reflect.TypeOf(y))                      // []int
-	fmt.Println(reflect.TypeOf(y) == reflect.TypeOf(x)) // true
-
-	fmt.Println(reflect.ValueOf(x).Kind()) // slice
-	fmt.Println(reflect.ValueOf(y).Kind()) // slice
-
-	var v []int
-	z := make([]int, 0, 10)
-
-	fmt.Println(v, len(v), cap(v))
-	fmt.Println(z, len(z), cap(z))
-
-	v = append(v, 3, 5, 7)
-	z = append(x, 3, 5, 7)
-
-	fmt.Println(v, len(v), cap(v))
-	fmt.Println(z, len(z), cap(z))
-
+	fmt.Println(x, a, b, c, d, e)
 	fmt.Println("Before Programm End")
+
 }
